cb777bf749647d3170be1478186342affb8d006a
Fefo <fefo_f93@hotmail.com>
2016-02-23 14:57:44 -0300

    testeo

--- Config.hs
+++ Config.hs
@@ -7,17 +7,15 @@ config = [
 
 ---------- Editar desde aquí...
 
-          -- NoiseGate 1,
-           --CompRel 40 5,
-           --NoiseGate 2,
-           --SetVolMax,
-           --SoftClipRel 1 10,
-          --, CompAvg 70
-          Tremolo 500 1 0.5,
-          --Panning 200 1 0.5,
-          --Echo 100 10 20,
-          --Echo 500 2 80,
-          SetVolMax
+          SetVolMax,
+          --NoiseGate 1,
+          --CompRel 40 5,
+          --SoftClipRel 5 30,
+          --CompAvg 10,
+          --Tremolo 500 1 0.5,
+          --Panning 200 1 0,
+          Echo 100 10 20
+          --SetVolMax
           --SetVolRel 80
           
 ---------- ...hasta aquí.
@@ -25,3 +23,20 @@ config = [
          ]
          
 -- Los efectos se aplican en el orden en que están escritos en la lista.
+{-
+Efectos para usar:
+data Efecto = SetVolMax
+            | SetVolRel Double
+            | NoiseGate Double
+            | ClipRel Double
+            | ClipAbs Sample
+            | SoftClipRel Double Double
+            | SoftClipAbs Sample Double
+            | CompRel Double Double
+            | CompAvg Double
+            | CompAbs Sample Double
+            | Tremolo Double Double Double
+            | Panning Double Double Double
+            | Delay Double Int Double
+            | Echo Double Int Double
+-}
